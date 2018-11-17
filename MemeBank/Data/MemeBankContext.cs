82e43c88815027808db6a5bc6785a679c4d18485
Natalia Usoltseva <usolnatalia@gmail.com>
2018-11-18 11:28:22 +1300

    Add project files.

---
+++ MemeBank/Data/MemeBankContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MemeBank.Modules;
+
+namespace MemeBank.Models
+{
+    public class MemeBankContext : DbContext
+    {
+        public MemeBankContext (DbContextOptions<MemeBankContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MemeBank.Modules.MemeItem> MemeItem { get; set; }
+    }
+}
---
+++ MemeBank/Modules/MemeImageItem.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeBank.Modules
+{
+    public class MemeImageItem
+    {
+
+        public string Title { get; set; }
+        public string Tags { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
